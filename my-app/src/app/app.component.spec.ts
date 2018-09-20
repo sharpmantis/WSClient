b3474d098915ef94c5e90c7bcbf2d5d2f4f2a480
sharpmantis <goursollas.emmanuel@gmail.com>
2018-09-20 11:51:54 +0200

    client 1.0

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'WSClient'`, async(() => {
+  it(`should have as title 'my-app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('WSClient');
+    expect(app.title).toEqual('my-app');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to WSClient!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
   }));
 });
---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'my-app';
+
+
+  //instance d'un observable WebSocket
+  private _socket: WebSocketSubject<any>;
+
+  //Tableau pour recevoir les messages du serveur
+  public serverMessages: any[];
+
+  constructor() {
+    console.log('Connexion client WebSocket');
+
+    this._socket = new WebSocketSubject('ws:127.0.0.1:8999');
+
+    //initialise le tableau des messages
+    this.serverMessages = [];
+
+    //Juste pour tester la comm sortante
+    //this._send();
+
+    this._socket
+      .subscribe((message) => {
+        console.log('le serveur envoie ' + JSON.stringify(message));
+        this.serverMessages.push(message);
+      },
+        (err) => console.error('erreur levée: ' + JSON.stringify(err)),
+        () => console.warn('completed')
+      );
+  }
+
+  private _send(): void {
+  console.log('envoie un nouveau message vers le serveur');
+  this._socket.next('Bonjour Serveur, je peux jouer?');
+  }
+
+}
